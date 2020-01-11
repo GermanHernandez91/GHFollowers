b66f1bbe490b58d05f11b58170ad3d5797500b63
German Hernandez <german220291@gmail.com>
2020-01-11 15:33:46 +0000

    Follower List
    Added Follower Collection Cell

---
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,34 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by German Hernandez on 11/01/2020.
+//  Copyright © 2020 German Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+    
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
