cb21242d34eed5340292572d3a41aa0fc71e43df
German Hernandez <german220291@gmail.com>
2020-01-11 23:40:17 +0000

    Empty State
    Added reusable empty state view controller if user has not followers

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -66,6 +66,12 @@ class FollowerListVC: UIViewController {
             case .success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
                 self.followers.append(contentsOf: followers)
+                
+                if self.followers.isEmpty {
+                    let message = "This user doesn't have any follower. Go follow them 😄."
+                    DispatchQueue.main.async { self.showEmptyStateView(with: message, in: self.view) }
+                    return
+                }
                 self.updateData()
                 
             case .failure(let error):
