677f84f0026ae723ef6b467867b8df9769f949a8
German Hernandez <german220291@gmail.com>
2020-01-11 13:26:11 +0000

    Custom Alert View
    Added custom alert view and showed up on username error

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -43,7 +43,7 @@ class SearchVC: UIViewController {
     @objc func pushFollowerListVC() {
         
         guard isUsernameEntered else {
-            print("No username")
+            pressentGFAlertOnMainThread(title: "Empty Username", message: "Please enter an username. We need to know who to look for 😄.", buttonTitle: "Ok")
             return
         }
         
--- GHFollowers/UIViewController+Ext.swift
+++ GHFollowers/UIViewController+Ext.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2020 German Hernandez. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func pressentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            
+            self.present(alertVC, animated: true)
+        }
+    }
+    
+}
