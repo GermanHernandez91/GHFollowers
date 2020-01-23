8f00308f841e8c2cc74374bd99c848840b56238a
German Hernandez <german220291@gmail.com>
2020-01-23 21:49:19 +0000

    Added user info header view

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by German Hernandez on 23/01/2020.
+//  Copyright © 2020 German Hernandez. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
+
